73e19caa5f4081f90017c8b07e9603df7046c7e1
Lucas Kovács <lucas.kovacs@gmail.com>
2016-10-19 21:31:28 -0300

    + New buddy page
    - Backed up old buddy page.-
    - Added new controller, model and view.-

---
+++ src/xgp3.0.0/upload/application/views/buddy/buddy_main_view.php
@@ -0,0 +1,33 @@
+<br>
+<div id="content">
+    <center>
+        <table width="519">
+            <tbody>
+                <tr>
+                    <td class="c" colspan="6">Buddylist</td>
+                </tr>
+                <tr>
+                    <th colspan="6"><a href="?page=buddy&amp;a=1">Request</a></th>
+                </tr>
+                <tr>
+                    <th colspan="6"><a href="?page=buddy&amp;a=1&amp;e=1">Your requests</a></th>
+                </tr>
+                <tr>
+                    <td class="c"></td>
+                    <td class="c">Name</td>
+                    <td class="c">Alliance</td>
+                    <td class="c">Coords</td>
+                    <td class="c">Status</td>
+                    <td class="c"></td>
+                </tr>
+                <tr>
+                    <th colspan="6">no entries</th>
+                </tr>
+        </tbody>
+        </table>
+        <br>
+        <br>
+        <br>
+        <br>
+    </center>
+</div>
---
+++ src/xgp3.0.0/upload/application/views/buddy/buddy_requests_view.php
@@ -0,0 +1,24 @@
+<br>
+<div id="content">
+    <center>
+        <table width="519">
+            <tbody>
+                <tr>
+                    <td class="c" colspan="6">Player requests</td>
+                </tr>
+
+                <tr>
+                    <th colspan="6">no entries</th>
+                </tr>
+
+                <tr>
+                    <td class="c" colspan="6"><a href="?page=buddy">back</a></td>
+                </tr>
+            </tbody>
+        </table>
+        <br>
+        <br>
+        <br>
+        <br>
+    </center>
+</div>
