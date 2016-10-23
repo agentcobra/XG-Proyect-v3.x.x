a51a93725734e10c20a5df54b67fe5fe88f38e8f
Lucas Kovács <lucas.kovacs@gmail.com>
2016-10-23 01:21:18 -0300

    Added several stuff to the template
    - Buddy points, and premium
    - Ignored players
    - Recruited buddies

--- src/xgp3.0.0/upload/application/views/buddy/buddy_main_view.php
+++ src/xgp3.0.0/upload/application/views/buddy/buddy_main_view.php
@@ -4,26 +4,118 @@
         <table width="519">
             <tbody>
                 <tr>
-                    <td class="c" colspan="6">Buddylist</td>
+                    <td class="c" colspan="6">{bu_title}</td>
                 </tr>
                 <tr>
-                    <th colspan="6"><a href="?page=buddy&amp;a=1">Request</a></th>
+                    <th colspan="6"><a href="?page=buddy&amp;a=1">{bu_request}</a></th>
                 </tr>
                 <tr>
-                    <th colspan="6"><a href="?page=buddy&amp;a=1&amp;e=1">Your requests</a></th>
+                    <th colspan="6"><a href="?page=buddy&amp;a=1&amp;e=1">{bu_your_requests}</a></th>
                 </tr>
                 <tr>
                     <td class="c"></td>
-                    <td class="c">Name</td>
-                    <td class="c">Alliance</td>
-                    <td class="c">Coords</td>
-                    <td class="c">Status</td>
+                    <td class="c">{bu_name}</td>
+                    <td class="c">{bu_alliance}</td>
+                    <td class="c">{bu_coords}</td>
+                    <td class="c">{bu_status}</td>
                     <td class="c"></td>
                 </tr>
                 <tr>
-                    <th colspan="6">no entries</th>
+                    <th colspan="6">{bu_no_entries}</th>
                 </tr>
-        </tbody>
+            </tbody>
+        </table>
+        <br>
+        <br>
+        <table width="519">
+            <tbody>
+                <tr>
+                    <td class="c" colspan="1">{bu_recruited}</td>
+                </tr>
+                <tr>
+                    <td colspan="1">
+                        {bu_invite_your_friends_msg}
+                        <br>
+                        <div align="right">
+                            <strong><a href="#">{bu_recruit_now}</a></strong>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <br>
+        <br>
+        <table width="519">
+            <tbody>
+                <tr>
+                    <td style="vertical-align: top">
+                        <table width="259">
+                            <tbody>
+                                <tr>
+                                    <td class="c" colspan="1">{bu_points_title}</td>
+                                </tr>
+                                <tr>
+                                    <td>&nbsp;</td>
+                                </tr>
+                                <tr height="75px">
+                                    <td style="vertical-align: top">
+                                        <ul>
+                                            <li>{bu_points_detail_1}</li>
+                                            <li>{bu_points_detail_2}</li>
+                                        </ul>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>{bu_points_legend}</td>
+                                </tr>
+                                <tr>
+                                     <td class="c">{bu_available}: 0 {bu_points_short}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                    <td style="vertical-align: top">
+                        <table width="259">
+                            <tbody>
+                                <tr>
+                                    <td class="c" colspan="2">{bu_premium_points_title}</td>
+                                </tr>
+                                <tr>
+                                    <td>&nbsp;</td>
+                                </tr>
+                                <tr height="75px">
+                                    <td style="vertical-align: top">
+                                        <img border="0" src="{dpath}resources/darkmatter.jpg" width="42" height="22" title="Dark Matter">
+                                    </td>
+                                    <td style="vertical-align: top">
+                                        {bu_premium_points_detail}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2">{bu_premium_points_legend}</td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2" class="c">{bu_available}: 0 {bu_premium_points_short}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <br>
+        <br>
+        <table width="519">
+            <tbody>
+                <tr>
+                    <td class="c">{bu_ignored_players}</td>
+                </tr>
+                <tr>
+                    <th>
+                        {bu_ignoring_no_one}
+                    </th>
+                </tr>
+            </tbody>
         </table>
         <br>
         <br>
