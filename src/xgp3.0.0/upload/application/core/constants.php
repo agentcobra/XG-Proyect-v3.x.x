809d38e53164b8c18f11c75da10053ffa8ca7905
Lucas Kovács <lucas.kovacs@gmail.com>
2016-10-23 01:51:06 -0300

    Changed version

--- src/xgp3.0.0/upload/application/core/constants.php
+++ src/xgp3.0.0/upload/application/core/constants.php
@@ -18,7 +18,7 @@
 #
 ###########################################################################
 // GAME FILES VERSION
-define('SYSTEM_VERSION', '3.0.2');
+define('SYSTEM_VERSION', '3.1.0');
 
 // HOOKS
 define('HOOKS_ENABLED', false);
