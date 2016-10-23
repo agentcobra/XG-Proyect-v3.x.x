adc5518522b9394d16c3e5dcd8c238c17d3d87e6
Lucas Kovács <lucas.kovacs@gmail.com>
2016-10-23 01:21:26 -0300

    Added basic model

--- src/xgp3.0.0/upload/application/models/game/buddy.php
+++ src/xgp3.0.0/upload/application/models/game/buddy.php
@@ -46,6 +46,84 @@ class Buddy
     {
         $this->db->closeConnection();
     }
+    
+    /**
+     * Add a new buddy
+     * 
+     * @param array $data Data to insert
+     * 
+     * @return mixed
+     */
+    public function addBuddy($data)
+    {
+        if (is_array($data)) {
+
+            $fields = '';
+            $values = '';
+            
+            foreach ($data as $key => $value) {
+
+                $fields .=  $key . ', ';
+                $values .=  '\'' . $value . '\', ';
+            }
+            
+            if ($this->db->query(
+                "INSERT INTO `" . BUDDY . "`(" . substr($fields, 0, -2) . ") VALUES(" . substr($values, 0, -2) . ")"
+            )) {
+                
+                return $this->db->insertId();
+            } else {
+                
+                return null;
+            }
+        }
+        
+        return null;
+    }
+    
+    /**
+     * Delete a buddy by the row ID
+     * 
+     * @param int $buddy_id Buddy ID
+     * 
+     * @return mixed
+     */
+    public function deleteBuddyById($buddy_id)
+    {
+        if ((int)$buddy_id > 0) {
+
+            return $this->db->query(
+                "DELETE * 
+                    FROM `" . BUDDY . "` 
+                    WHERE `buddy_id` = '" . $buddy_id . "'"
+            );
+        }
+        
+        return null;
+    }
+    
+    /**
+     * Delete a buddy by the row ID
+     * 
+     * @param int $sender_id   Who requested ID
+     * @param int $receiver_id Who received the request ID
+     * 
+     * @return mixed
+     */
+    public function deleteBuddy($sender_id, $receiver_id)
+    {
+        if ((int)$sender_id > 0 && (int)$receiver_id > 0) {
+
+            return $this->db->query(
+                "DELETE * 
+                    FROM `" . BUDDY . "` 
+                    WHERE `buddy_sender` = '" . $sender_id . "' 
+                        AND `buddy_receiver` = '" . $receiver_id . "'"
+            );
+        }
+        
+        return null;
+    }
 }
 
 /* end of buddy.php */
